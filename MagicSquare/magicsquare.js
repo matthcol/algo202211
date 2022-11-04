6bb107298aaab3c7793acc0ca600ff6c87ee7444
matthcol <matthias.colin@gmail.com>
2022-11-04 14:33:24 +0100

    square magic prototypes

---
+++ MagicSquare/magicsquare.js
@@ -0,0 +1,44 @@
+function isMagic(square){
+    // 1. compute magical sum: n(n^2+1)/2
+    // 2. check each row
+    // 3. check each column
+    // 4. les "diagonales"
+    // TODO 5. use all numbers from 1 to n^2 (once each)
+    return false
+}
+
+function build(n){
+    return [[1, 1], 
+            [1, 2]]
+}
+
+const squareOk = 
+    [[2, 7, 6],
+     [9, 5, 1],
+     [4, 3, 8]]
+
+// rows 1 & 3 are Ko
+const squareKoRow = 
+     [[2, 3, 6],
+      [9, 5, 1],
+      [4, 7, 8]]
+
+const squareKoColumn = 
+      [[2, 7, 6],
+       [1, 5, 9],
+       [4, 3, 8]]
+  
+const squareKoDiag = 
+       [[2, 7, 6],
+        [4, 3, 8],
+        [9, 5, 1]]
+    
+
+const ok = isMagic(squareOk)
+// console.log(squareOk)
+console.log("square ok", ok)
+
+const koR = isMagic(squareKoRow)
+const koC = isMagic(squareKoColumn)
+const koD = isMagic(squareKoDiag)
+console.log("squares ko", koR, koC, koD)
